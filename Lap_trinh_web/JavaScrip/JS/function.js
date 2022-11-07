d527aa4f4304e4c26b2be6f786a08399f081612b
vanthuc1308 <20010927@st.phenikaa-uni.edu.vn>
2022-11-07 16:37:41 +0700

    07_11

--- Lap_trinh_web/JavaScrip/JS/function.js
+++ Lap_trinh_web/JavaScrip/JS/function.js
@@ -1,23 +1,58 @@
 
-function ptb1(a, b) {
-    var a = prompt("nhập a")
-    var b = prompt("nhập b")
-    if (a == 0) {
-        if (b == 0) {
-            alert("Phương trình vô số nghiệm");
-        } else {
-            alert( "Phương trình vô nghiệm");
-        }
-    } else {
-        alert ("Phương trình có nghiệm x = " + (-b / a));
+
+
+// function ptb1(a,b)
+// {
+//     var a = document.getElementById('a').value;
+// var b = document.getElementById('b').value;
+// var result = document.getElementsByClassName('mybox')[0];
+// a=Number(a);
+// b=Number(b);
+//     if (a == 0 && b == 0) {
+//         result.innerHTML='Phuong trinh vo so nghiem';
+//     } else if (a != 0 && b == 0) {
+//         result.innerHTML='Phương trình có nghiệm x = 0';
+//     } else if (a == 0 && b != 0) {
+//         result.innerHTML="Phương trình vô nghiệm";
+//     } else {
+//         result.innerHTML='Phương trình' +a+'x +'+b+'  có nghiệm x = ' + (-b / a);
+//     }
+// }
+// // caculator
+// function caculator() {
+//     var result = document.querySelectorAll('.btn1')
+//         for (var i = 0; i < btn.length; i++) {
+//             btn[i].onclick = function() {
+//                 thisObject.values(input.innerHTML);
+
+numbers = document.querySelectorAll('.btn')
+result = document.getElementById('result')
+
+for (var i = 0; i < numbers.length; i++) {
+    numbers[i].onclick = function() {
+        result.value += this.value
     }
 }
 
-x = sumAll(1,23,500,115,44,88);
-function sumAll(){
-    var i,sum = 0;
-    for (i = 0; i <arguments.length; i++){
-        sum += arguments[i];
-    }
-    alert("Tổng các số là: " + sum);
-}
+equal = document.getElementsByClassName("equal")[0]
+equal.addEventListener("click",function() {
+    result.value = eval(result.value)
+})
+
+clear = document.getElementsByClassName("clear")[0]
+clear.addEventListener("click",function() {
+    result.value= ""
+})
+
+
+
+
+        
+
+
+
+
+
+
+
+
